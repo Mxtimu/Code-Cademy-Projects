a0e3ab88f2c2d5d2ceeb167f35083542a2be06a8
mxtimu <matimujunior3@gmail.com>
2025-07-16 21:57:50 +0200

    Addition: added an update score function to update the score

--- number-guesser/script.js
+++ number-guesser/script.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+
 const generateTarget = () => {
 return Math.floor(Math.random() * 10);
 }
@@ -13,6 +13,13 @@ const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
   return humanDifference <= computerDifference ;
 }
 
+const updateScore = winner => {
+  if (winner === 'human' ){
+      humanScore++;
+  } else if (winner === 'computer') {
+      computerScore++;
+  } 
+}
 
 
 console.log(generateTarget());
